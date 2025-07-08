dd6c1e07f267b662d993130df5b9c1937591545f
230660221002 <230660221002@student.unsap.ac.id>
2025-07-08 22:45:39 +0700

    Menambahkan direktori task-manager

---
+++ task-manager/tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+ // tailwind.config.js
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+}
